90099ce8a93183f79e3aeae5693d2e1ce6d21ef0
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2019-05-27 22:45:42 -0300

    Testando a classe Categoria

--- src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rafael/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.rafael.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return	"Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1  = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2 , "Escritorio");
+		
+		List<Categoria> listaCategorias = new ArrayList<Categoria>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
+		
 	}
 	
 
